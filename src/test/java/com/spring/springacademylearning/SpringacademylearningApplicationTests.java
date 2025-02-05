e06fc9fdb4ecc9ac48d95f8572b9f7b6b6e7b96a
David Prasad <d7prasad89@gmail.com>
2025-02-04 19:53:07 -0500

    Small refactor

--- src/test/java/com/spring/springacademylearning/SpringacademylearningApplicationTests.java
+++ src/test/java/com/spring/springacademylearning/SpringacademylearningApplicationTests.java
@@ -4,26 +4,38 @@ import com.jayway.jsonpath.DocumentContext;
 import com.jayway.jsonpath.JsonPath;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.web.servlet.assertj.MockMvcTester;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
 class SpringacademylearningApplicationTests {
 
+	@Autowired
+	MockMvcTester mockMvcTester;
 	@Autowired
 	TestRestTemplate restTemplate;
 
 	@Test
 	void contextLoads() {
+		assertThat(mockMvcTester.get().uri("/api/v1/cashcard"))
+				.hasStatusOk()
+				.hasContentTypeCompatibleWith(MediaType.TEXT_PLAIN)
+				.bodyJson()
+				.convertTo(String.class)
+				.satisfies(s -> assertThat(s).isEqualTo("cashcard"));
 		ResponseEntity<String> response = restTemplate.getForEntity("/api/v1/cashcard/99", String.class);
 		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
 
 		DocumentContext documentContext = JsonPath.parse(response.getBody());
-		Number id = documentContext.read("$.id");
+		Number id = documentContext.read("$.Id");
 		assertThat(id).isEqualTo(99);
 
 		Double amount = documentContext.read("$.amount");
